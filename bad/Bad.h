0efd2629dab624ddd1754f929875d4371dfb0d28
tokudiro <tokudiro@gmail.com>
2016-05-12 22:08:15 +0900

    del file

--- bad/Bad.h
+++
@@ -1,9 +0,0 @@
-#ifndef __Bad_H__
-#define __Bad_H__
-/* class Bad */
-typedef struct Bad_struct Bad;
-Bad* Bad_getInstance();
- ;
-int Bad_get_z(const Bad* this);
-A* Bad_get_a(const Bad* this);
-#endif /* __Bad_H__ */
--- bad/Bad_define.h
+++
@@ -1,8 +0,0 @@
-#include "Bad.h"
-/* class Bad */
-struct Bad_struct{
-int b[10] ;;
-int c[10];
- int z;
- A* a;
-};
