ff5ccb2eae5f106c076f49fd40a80b820b4c3961
tokudiro <tokudiro@gmail.com>
2016-05-21 01:29:06 +0900

    update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,17 @@
 cd ..
 ./build.sh
+
 cd test
+rm *.c
+rm *.h
+
 ../caos -L test_METHOD_SCOPE_01.caos
-gcc test_METHOD_SCOPE_01.c -o test_METHOD_SCOPE_01.exe
+gcc test_METHOD_SCOPE_01.c -o test_METHOD_SCOPE_01
 
 ../caos -L test_METHOD_DEFINE_01.caos
-gcc test_METHOD_DEFINE_01.c -o test_METHOD_DEFINE_01.exe
+gcc test_METHOD_DEFINE_01.c -o test_METHOD_DEFINE_01
 ./test_METHOD_DEFINE_01
 
 ../caos -L test_METHOD_DEFINE_02.caos
-gcc test_METHOD_DEFINE_02.c -o test_METHOD_DEFINE_02.exe
+gcc test_METHOD_DEFINE_02.c -o test_METHOD_DEFINE_02
 ./test_METHOD_DEFINE_02
